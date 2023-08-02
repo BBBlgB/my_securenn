a7e5175a488a966ae39b3f01e36fffc2260e590a
BBBlgB <BBBlgB@bowen.shen@student-cs.fr>
2023-08-02 16:52:55 +0800

    just a test: changing the clang-format of lib

--- libspu/mpc/securenn/type_test.cc
+++ libspu/mpc/securenn/type_test.cc
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-#include "libspu/mpc/securenn/type.h"
+#include <libspu/mpc/securenn/type.h>
 
 namespace spu::mpc::securenn {
 
